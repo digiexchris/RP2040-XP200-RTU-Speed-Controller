8bf8c886ea8e45397e275b9f820cd7cb45090a68
Chris Chatelain <chris.chatelain@digiex.ca>
2025-04-25 16:38:44 -0500

    Start integrating zephyr modbus client

--- XP200_RTU_HID/src/main.cpp
+++ XP200_RTU_HID/src/main.cpp
@@ -11,8 +11,14 @@
 #include <zephyr/sys/util.h>
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
+#include <zephyr/device.h>
+#include <zephyr/modbus/modbus.h>
+
 #include "ui/buttons.hpp"
 #include "modbus/CommandQueue.hpp"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 #define SLEEP_TIME_MS 1
 
@@ -35,15 +41,116 @@
 //     {GPIO_DT_SPEC_GET_OR(EStop, gpios, {0}), Action::EStop},
 // };
 
-void button_pressed(const struct device *dev, struct gpio_callback *cb,
-                    uint32_t pins)
+// const struct device *uart0 = DEVICE_DT_GET(DT_NODELABEL(uart0));
+static int client_iface;
+
+const static struct modbus_iface_param client_param = {
+    .mode = MODBUS_MODE_RTU,
+    .rx_timeout = 50000,
+    .serial = {
+        .baud = 19200,
+        .parity = UART_CFG_PARITY_NONE,
+        .stop_bits_client = UART_CFG_STOP_BITS_2,
+    },
+};
+
+#define MODBUS_NODE DT_NODELABEL(modbus0)
+
+static int init_modbus_client(void)
 {
-    printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+    const char *iface_name = DEVICE_DT_NAME(MODBUS_NODE);
+    client_iface = modbus_iface_get_by_name(iface_name);
+    return modbus_init_client(client_iface, client_param);
 }
 
 int main(void)
 {
-    int ret;
+    uint16_t holding_reg[8] = {'H', 'e', 'l', 'l', 'o'};
+    const uint8_t coil_qty = 3;
+    uint8_t coil[1] = {0};
+    const int32_t sleep = 250;
+    static uint8_t node = 1;
+    int err;
 
-    return 0;
+    if (init_modbus_client())
+    {
+        LOG_ERR("Modbus RTU client initialization failed");
+        return 0;
+    }
+
+    err = modbus_write_holding_regs(client_iface, node, 0, holding_reg,
+                                    ARRAY_SIZE(holding_reg));
+    if (err != 0)
+    {
+        LOG_ERR("FC16 failed with %d", err);
+        return 0;
+    }
+
+    err = modbus_read_holding_regs(client_iface, node, 0, holding_reg,
+                                   ARRAY_SIZE(holding_reg));
+    if (err != 0)
+    {
+        LOG_ERR("FC03 failed with %d", err);
+        return 0;
+    }
+
+    LOG_HEXDUMP_INF(holding_reg, sizeof(holding_reg),
+                    "WR|RD holding register:");
+
+    while (true)
+    {
+        uint16_t addr = 0;
+
+        err = modbus_read_coils(client_iface, node, 0, coil, coil_qty);
+        if (err != 0)
+        {
+            LOG_ERR("FC01 failed with %d", err);
+            return 0;
+        }
+
+        LOG_INF("Coils state 0x%02x", coil[0]);
+
+        err = modbus_write_coil(client_iface, node, addr++, true);
+        if (err != 0)
+        {
+            LOG_ERR("FC05 failed with %d", err);
+            return 0;
+        }
+
+        k_msleep(sleep);
+        err = modbus_write_coil(client_iface, node, addr++, true);
+        if (err != 0)
+        {
+            LOG_ERR("FC05 failed with %d", err);
+            return 0;
+        }
+
+        k_msleep(sleep);
+        err = modbus_write_coil(client_iface, node, addr++, true);
+        if (err != 0)
+        {
+            LOG_ERR("FC05 failed with %d", err);
+            return 0;
+        }
+
+        k_msleep(sleep);
+        err = modbus_read_coils(client_iface, node, 0, coil, coil_qty);
+        if (err != 0)
+        {
+            LOG_ERR("FC01 failed with %d", err);
+            return 0;
+        }
+
+        LOG_INF("Coils state 0x%02x", coil[0]);
+
+        coil[0] = 0;
+        err = modbus_write_coils(client_iface, node, 0, coil, coil_qty);
+        if (err != 0)
+        {
+            LOG_ERR("FC15 failed with %d", err);
+            return 0;
+        }
+
+        k_msleep(sleep);
+    }
 }
